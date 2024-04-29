1d2e53cb51e95f0c77473843e3cc7f990e87a5db
Tomer-Shor1 <156070203+Tomer-Shor1@users.noreply.github.com>
2024-04-29 16:45:25 +0300

    Part B

--- RUDP_Receiver.c
+++ RUDP_Receiver.c
@@ -9,7 +9,7 @@
 
 
 int main(int argc, char* argv[]){
-    int port = atoi(argv[2]);  //convert port string into integer
+    int port = atoi(argv[2]);  // convert port string into integer
     printf("Server's port number is %d\n", port);
 
     printf("-------- RUDP Receiver --------\n");
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]){
         return ERROR;
     }
 
-    //connect the socket and port
+    // Listen and save client address
     int rc = RUDP_listen(socket, port);
     if (rc == -1) {
         printf("Failed to connect\n");
@@ -29,75 +29,69 @@ int main(int argc, char* argv[]){
 
     RUDP * packet = (RUDP*)malloc(sizeof(RUDP));
     if (packet == NULL) {
-        return -1;
+        return ERROR;
     }  
 
-    //create file for saving the stats
+    // create file for saving the stats
     FILE* file = fopen("stats", "w+");
     if (file == NULL) {
-    printf("Error opening  stats file\n");
-    return 1;
+        printf("Error opening  stats file\n");
+        return ERROR;
     }
 
     fprintf(file, "\n\n~~~~~~~~ Stats ~~~~~~~~\n");
-    double average_time = 0;
-    double average_speed = 0;
-    clock_t start, end;
+    double avg_time = 0;
+    double avg_speed = 0;
+    clock_t start_time, end_time;
 
     char* data = NULL;
-    int data_length = 0;
     char total_data[2097152] = {0};  //2MB
+    int data_length = 0;
 
-    start = clock();
-    end = clock();
+    start_time = clock();
+    end_time = clock();
     int number_of_runs = 0;
 
     do {
         int rc = RUDP_receive(socket, &data, &data_length);
         if (rc == -1) {
-            printf("error. failed to receive\n");
+            printf("Error. failed to receive\n");
             return ERROR;
         }
-
-        if (rc == -2) {  //if the connection was closed by the sender
+        if (rc == -2) {  // if the connection was closed by the sender - break and print stats.
             break;
         }
-
-        if (rc == 1 && start < end) {  // if its the first data packet, start the timer.
-            start = clock();
+        if (rc == 1 && start_time < end_time) {  // if its the first data packet, start the timer.
+            start_time = clock();
         }
-
-        if (rc == 1) {  //if its data packet, add it to the total data
+        if (rc == 1) {  // if its data packet, add it to the total data
             strcat(total_data, data);
         }
-
-        if (rc == 2) { //if received a last packet of data
+        if (rc == 2) { // if received a last packet of data
             strcat(total_data, data);
-            printf("Received total data: %zu\n", sizeof(total_data));
-            end = clock();
-            double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
-            average_time += time_taken;
+            printf("Received all data\n");
+            end_time = clock();
+            double time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
             double speed = 2 / time_taken;
-            average_speed += speed;
-            fprintf(file, "Run #%d Data: Time=%f S ; Speed=%f MB/S\n", number_of_runs,
-              time_taken, speed);  //write stats to the file
-            memset(total_data, 0, sizeof(total_data));  
+            avg_time += time_taken;
+            avg_speed += speed;
+            fprintf(file, "Run #%d Time=%f S ; Speed=%f MB/S\n", number_of_runs, time_taken, speed);  // write stats to the file
+            memset(total_data, 0, sizeof(total_data));  // initilize data
             number_of_runs++;
         }
     }while (rc >= 0);
 
-    //add stats to the file
+    // add stats to the file
     fprintf(file, "\n");
-    fprintf(file, "Average time: %f S\n", average_time / (number_of_runs - 1));
-    fprintf(file, "Average speed: %f MB/S\n", average_speed / (number_of_runs - 1));
+    fprintf(file, "Average time: %f S\n", avg_time / (number_of_runs - 1));
+    fprintf(file, "Average speed: %f MB/S\n", avg_speed / (number_of_runs - 1));
 
     //print the file
     rewind(file);
     char print_buffer[100];
-    while (fgets(print_buffer, 100, file) != NULL) {
+    while (fgets(print_buffer, 100, file) != NULL){
     printf("%s", print_buffer);
     }
-
     fclose(file);
     return 0;
 }
